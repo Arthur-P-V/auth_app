3dce4f2efaa55134df6381089e0d9f50d1d5cb64
Arthur PV <apetersonveatch@gmail.com>
2020-11-10 20:52:05 -0600

    initial commit, adds homework from tuesday

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,25 @@
+class Api::UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    if current_user
+      id = params["id"]
+      @user = User.find(id)
+      render "show.json.jb"
+    else
+      render json: { message: "must be logged in" }
+    end
+  end
+end
